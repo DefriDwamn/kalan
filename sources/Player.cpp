cb0a9a25292cc9efab25ea7acd931c2954a2a7d0
Novashinskiy.Y.M <emailno@bk.ru>
2025-03-25 02:43:40 +0300

    feat: add matrix logic with hands model

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -1,25 +1,65 @@
+#include <memory>
+
 #include "Player.hpp"
 #include "raylib-cpp.hpp"
 
-Player::Player() {
-  position = Rectangle{GetScreenWidth() / 2.0f - 50,
-                       GetScreenHeight() / 2.0f - 50, 100, 100};
-  speed = 3;
+Player::Player(std::unique_ptr<raylib::Model> handsModel,
+               raylib::Camera3D* camera = nullptr,
+               raylib::Vector3 position = {0.}, int speed = 10)
+    : handsModel(std::move(handsModel)),
+      camera(camera),
+      position(position),
+      speed(speed) {}
+
+void Player::SetCamera(raylib::Camera3D* camera) { this->camera = camera; }
+
+void Player::SetHandsOffset(const raylib::Vector3& offset) {
+  handsOffset = offset;
 }
 
-void Player::Draw() { position.Draw(RED); }
+void Player::SetHandsRotation(const raylib::Vector3& rotation) {
+  handsRotation = rotation;
+}
+
+void Player::SetSmoothFactor(float smooth) { smoothFactor = smooth; }
+
+raylib::Vector3 Player::GetHandsOffset() const { return handsOffset; }
+
+raylib::Vector3 Player::GetHandsRotation() const { return handsRotation; }
+
+float Player::GetSmoothFactor() const { return smoothFactor; }
+
+void Player::UpdateHandsTransform() {
+  if (!camera || !handsModel) return;
+
+  raylib::Vector3 direction =
+      (raylib::Vector3(camera->target) - camera->position).Normalize();
+
+  targetHandsPos = raylib::Vector3(camera->position) + direction;
+  currHandsPos =
+      currHandsPos.Lerp(targetHandsPos, smoothFactor * GetFrameTime());
+
+  raylib::Matrix lookAtMatrix =
+      raylib::Matrix::LookAt(currHandsPos, camera->position, {0.0f, 1.0f, 0.0f})
+          .Invert();
+
+  raylib::Matrix rotationMatrix =
+      raylib::Matrix::RotateZ(handsRotation.z * DEG2RAD) *
+      raylib::Matrix::RotateY(handsRotation.y * DEG2RAD) *
+      raylib::Matrix::RotateX(handsRotation.x * DEG2RAD);
+
+  handsModel->transform = rotationMatrix * lookAtMatrix;
+  handsModel->transform =
+      raylib::Matrix::Translate(handsOffset.x, handsOffset.y, handsOffset.z) *
+      handsModel->transform;
+}
 
 void Player::Update() {
-  if (IsKeyDown(KEY_UP)) {
-    position.y -= speed;
-  }
-  if (IsKeyDown(KEY_DOWN)) {
-    position.y += speed;
-  }
-  if (IsKeyDown(KEY_RIGHT)) {
-    position.x += speed;
-  }
-  if (IsKeyDown(KEY_LEFT)) {
-    position.x -= speed;
-  }
+  if (!camera) return;
+  UpdateHandsTransform();
+}
+
+void Player::Draw() {
+  if (!camera || !handsModel) return;
+  handsModel->Draw({0}, 1.0f, RED);
 }
--- sources/Player.hpp
+++ sources/Player.hpp
@@ -1,10 +1,36 @@
+#pragma once
+#include <memory>
+
 #include "raylib-cpp.hpp"
 
 class Player {
- public:
-  Player();
-  raylib::Rectangle position;
+ private:
+  raylib::Vector3 position;
+  std::unique_ptr<raylib::Model> handsModel;
+  raylib::Camera3D* camera;
   int speed;
+
+  raylib::Vector3 handsOffset = {-1.1f, -0.7f, 2.0f};
+  raylib::Vector3 handsRotation = {-10.0f, 0.0f, 0.0f};
+  raylib::Vector3 targetHandsPos = {0};
+  raylib::Vector3 currHandsPos = {0};
+  float smoothFactor = 15;
+  void UpdateHandsTransform();
+
+ public:
+  Player(std::unique_ptr<raylib::Model> model, raylib::Camera3D* camera,
+         raylib::Vector3 position, int speed);
+
+  void SetCamera(raylib::Camera3D* camera);
+
+  void SetHandsOffset(const raylib::Vector3& offset);
+  void SetHandsRotation(const raylib::Vector3& rotation);
+  void SetSmoothFactor(float smooth);
+
+  raylib::Vector3 GetHandsOffset() const;
+  raylib::Vector3 GetHandsRotation() const;
+  float GetSmoothFactor() const;
+
   void Draw();
   void Update();
 };
