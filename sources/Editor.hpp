4909061c7f92fc641143be4fa56a7da0ce33ef97
Novashinskiy.Y.M <emailno@bk.ru>
2025-03-25 22:58:39 +0300

    feat(editor): add method for toggle editor and now steps gets from class member

--- sources/Editor.hpp
+++ sources/Editor.hpp
@@ -3,6 +3,7 @@
 #include "Player.hpp"
 #include "imgui.h"
 #include "raylib-cpp.hpp"
+#include "raylib.h"
 #include "rlImGui.h"
 
 class Editor {
@@ -11,55 +12,64 @@ class Editor {
       : player(player),
         handsOffset(player->GetHandsOffset()),
         handsRotation(player->GetHandsRotation()),
-        smoothFactor(player->GetSmoothFactor()) {}
+        smoothFactor(player->GetSmoothFactor()),
+        show(true) {}
 
   void Draw();
+  void ToggleShow();
 
  private:
+  float fastStep = 10.f;
+  float step = 1.f;
   Player* player;
-  raylib::Vector3 handsOffset = {0};
-  raylib::Vector3 handsRotation = {0};
-  float smoothFactor = 0.5f;
+  raylib::Vector3 handsOffset;
+  raylib::Vector3 handsRotation;
+  float smoothFactor;
+  bool show;
 };
 
+void Editor::ToggleShow() { show = !show; }
+
 void Editor::Draw() {
+  if (!show) return;
+
   rlImGuiBegin();
   if (ImGui::Begin("Player Settings")) {
     ImGui::Text("Hands Offset:");
-    ImGui::SameLine();
     ImGui::PushItemWidth(100);
-    if (ImGui::InputFloat("X##handsOffset", &handsOffset.x, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("X##handsOffset", &handsOffset.x, step, fastStep)) {
       player->SetHandsOffset(handsOffset);
     }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Y##handsOffset", &handsOffset.y, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("Y##handsOffset", &handsOffset.y, step, fastStep)) {
       player->SetHandsOffset(handsOffset);
     }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Z##handsOffset", &handsOffset.z, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("Z##handsOffset", &handsOffset.z, step, fastStep)) {
       player->SetHandsOffset(handsOffset);
     }
     ImGui::PopItemWidth();
 
     ImGui::Text("Hands Rotation:");
-    ImGui::SameLine();
     ImGui::PushItemWidth(100);
-    if (ImGui::InputFloat("X##handsRotation", &handsRotation.x, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("X##handsRotation", &handsRotation.x, step,
+                          fastStep)) {
       player->SetHandsRotation(handsRotation);
     }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Y##handsRotation", &handsRotation.y, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("Y##handsRotation", &handsRotation.y, step,
+                          fastStep)) {
       player->SetHandsRotation(handsRotation);
     }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Z##handsRotation", &handsRotation.z, 0.1f, 1.0f)) {
+    if (ImGui::InputFloat("Z##handsRotation", &handsRotation.z, step,
+                          fastStep)) {
       player->SetHandsRotation(handsRotation);
     }
     ImGui::PopItemWidth();
 
     ImGui::Text("Smooth Factor:");
-    ImGui::SameLine();
-    if (ImGui::SliderFloat("##SmoothFactor", &smoothFactor, 0.0f, 50.0f)) {
+    if (ImGui::SliderFloat("##SmoothFactor", &smoothFactor, 0.0f, 200.0f)) {
       player->SetSmoothFactor(smoothFactor);
     }
   }
