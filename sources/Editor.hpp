d99b849d284b010afb209c50c90c721173817be3
Novashinskiy.Y.M <emailno@bk.ru>
2025-03-27 03:05:28 +0300

    refactor: editor with singleton and add comments for future changes with entt

--- sources/Editor.hpp
+++ sources/Editor.hpp
@@ -8,18 +8,29 @@
 
 class Editor {
  public:
-  Editor(Player* player)
-      : player(player),
-        handsOffset(player->GetHandsOffset()),
-        handsRotation(player->GetHandsRotation()),
-        smoothFactor(player->GetSmoothFactor()),
-        show(true) {}
+  static Editor& GetInstance(Player* player = nullptr) {
+    static Editor inst(player);
+    return inst;
+  }
+  Editor(const Editor&) = delete;
+  Editor& operator=(const Editor&) = delete;
 
   void Draw();
   void ToggleShow();
   bool IsVisible();
+  void SetPlayer(Player* player) { this->player = player; }
 
  private:
+  Editor(Player* player)
+      : player(player),
+        handsOffset(player ? player->GetHandsOffset() : raylib::Vector3{0}),
+        handsRotation(player ? player->GetHandsRotation() : raylib::Vector3{0}),
+        smoothFactor(player ? player->GetSmoothFactor() : 50),
+        show(true) {
+    rlImGuiSetup(true);
+  }
+  ~Editor() { rlImGuiShutdown(); }
+
   float fastStep = 10.f;
   float step = 1.f;
   Player* player;
@@ -29,51 +40,41 @@ class Editor {
   bool show;
 };
 
-bool Editor::IsVisible() { return show; }
-void Editor::ToggleShow() { show = !show; }
+inline bool Editor::IsVisible() { return show; }
+inline void Editor::ToggleShow() { show = !show; }
 
-void Editor::Draw() {
+inline void Editor::Draw() {
   if (!show) return;
 
   rlImGuiBegin();
   if (ImGui::Begin("Player Settings")) {
     ImGui::Text("Hands Offset:");
     ImGui::PushItemWidth(100);
-    if (ImGui::InputFloat("X##handsOffset", &handsOffset.x, step, fastStep)) {
+    if (ImGui::InputFloat("X##handsOffset", &handsOffset.x, step, fastStep))
       player->SetHandsOffset(handsOffset);
-    }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Y##handsOffset", &handsOffset.y, step, fastStep)) {
+    if (ImGui::InputFloat("Y##handsOffset", &handsOffset.y, step, fastStep))
       player->SetHandsOffset(handsOffset);
-    }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Z##handsOffset", &handsOffset.z, step, fastStep)) {
+    if (ImGui::InputFloat("Z##handsOffset", &handsOffset.z, step, fastStep))
       player->SetHandsOffset(handsOffset);
-    }
     ImGui::PopItemWidth();
 
     ImGui::Text("Hands Rotation:");
     ImGui::PushItemWidth(100);
-    if (ImGui::InputFloat("X##handsRotation", &handsRotation.x, step,
-                          fastStep)) {
+    if (ImGui::InputFloat("X##handsRotation", &handsRotation.x, step, fastStep))
       player->SetHandsRotation(handsRotation);
-    }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Y##handsRotation", &handsRotation.y, step,
-                          fastStep)) {
+    if (ImGui::InputFloat("Y##handsRotation", &handsRotation.y, step, fastStep))
       player->SetHandsRotation(handsRotation);
-    }
     ImGui::SameLine();
-    if (ImGui::InputFloat("Z##handsRotation", &handsRotation.z, step,
-                          fastStep)) {
+    if (ImGui::InputFloat("Z##handsRotation", &handsRotation.z, step, fastStep))
       player->SetHandsRotation(handsRotation);
-    }
     ImGui::PopItemWidth();
 
     ImGui::Text("Smooth Factor:");
-    if (ImGui::SliderFloat("##SmoothFactor", &smoothFactor, 0.0f, 200.0f)) {
+    if (ImGui::SliderFloat("##SmoothFactor", &smoothFactor, 0.0f, 200.0f))
       player->SetSmoothFactor(smoothFactor);
-    }
   }
   ImGui::End();
   rlImGuiEnd();
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,14 @@
 #include "Editor.hpp"
-#include "Keyboard.hpp"
 #include "Player.hpp"
-#include "raylib.h"
+#include "entt/entity/fwd.hpp"
+#include "entt/entt.hpp"
+#include "raylib-cpp.hpp"
 
 int main() {
   raylib::Window window(1920, 1080, "Kalan");
+
+  entt::registry registry;
+
   raylib::Camera camera({0.2f, 0.4f, 0.2f}, {0.0f, 0.0f, 0.0f},
                         {0.0f, 1.0f, 0.0f}, 45.0f);
 
@@ -12,13 +16,13 @@ int main() {
       std::make_unique<raylib::Model>(raylib::MeshUnmanaged::Cube(1., 1., 1.));
   Player player(std::move(playerModel), &camera, {0.}, 10);
 
-  Editor editor(&player);
-  rlImGuiSetup(true);
-
+  Editor& editor = Editor::GetInstance(&player);
   SetExitKey(0);
 
   while (!window.ShouldClose()) {
     // Updating
+
+    // entity in InputSystem should update
     if (raylib::Keyboard::IsKeyPressed(KEY_F11)) window.ToggleFullscreen();
     if (raylib::Keyboard::IsKeyPressed(KEY_F10)) {
       editor.ToggleShow();
@@ -27,31 +31,34 @@ int main() {
       else
         DisableCursor();
     }
+    //
 
+    // entity with TrasformComponent should update
     if (!editor.IsVisible()) camera.Update(CameraMode::CAMERA_FIRST_PERSON);
     player.Update();
+    //
 
     // Drawing
     BeginDrawing();
     {
       window.ClearBackground(RAYWHITE);
 
-      // 3D Drawing 
+      // entity with DrawableComponent 3d should update
       camera.BeginMode();
       {
         player.Draw();
         DrawGrid(100, 1);
         DrawCube({0}, 2.0f, 2.0f, 2.0f, YELLOW);
       }
-
-      // 2D Drawing
       camera.EndMode();
+      //
+
+      // entity with DrawableComponent 2d should update
       window.DrawFPS(0, 0);
       editor.Draw();
+      //
     }
     EndDrawing();
   }
-
-  rlImGuiShutdown();
   return 0;
 }
